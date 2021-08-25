85ce158361a23ce9f7418985bdbc840e3575cfe4
mvrakesh341 <89510137+mvrakesh341@users.noreply.github.com>
2021-08-25 20:49:21 +0530

    Add files via upload

---
+++ array of food data.js
@@ -0,0 +1,25 @@
+let food_menu=[{
+    Name:"Idly",
+    Price:30,
+    Quantity:4
+},{
+    Name:"Dosa",
+    Price:40,
+    Quantity:1
+},{
+    Name:"Puri",
+    Price:30,
+    Quantity:2
+},{
+    Name:"Vada",
+    Price:10,
+    Quantity:1
+},{
+    Name:"Roti",
+    Price:30,
+    Quantity:1
+}]
+//console.log(food_menu[0])
+for(let i=0;i<food_menu.length;i++){
+    console.log(food_menu[i])
+}
